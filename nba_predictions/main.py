3028461c074f5e5c08cf482fb140fed762a84cfe
lemon-lyman <nicholaswest@utexas.edu>
2019-10-30 01:58:57 -0500

    sys user pull override feature

--- nba_predictions/main.py
+++ nba_predictions/main.py
@@ -3,6 +3,7 @@ from dashboard import create_dashboard
 import models
 import datetime
 import numpy as np
+import sys
 
 
 ## Goal: Monitor ongoing performance of odds, FTE, personal models, etc.
@@ -10,15 +11,19 @@ import numpy as np
 if __name__ == "__main__":
 
     po = False
+    if len(sys.argv) > 1:
+        if "po" in sys.argv:
+            po = True
 
     record = create_record(pull_override=po)
+
+    nick = models.create_user_model(record, "Nick")
+    carlos = models.create_user_model(record, "Carlos")
+    koen = models.create_user_model(record, "Koen")
 
     raptor = models.create_raptor_model(record, pull_override=po)
     elo = models.create_elo_model(record, pull_override=po)
     lvi = models.create_lvi_model(record)
-    nick = models.create_user_model(record, "Nick")
-    carlos = models.create_user_model(record, "Carlos")
-    koen = models.create_user_model(record, "Koen")
 
     create_dashboard(nick,
                      carlos,
