3f08e38e815d6bba97c4eda4915ec0de2eb2ddbf
lemon-lyman <nicholaswest@utexas.edu>
2019-10-29 19:03:57 -0500

    testing lvi stuff

--- nba_predictions/scratch.py
+++ nba_predictions/scratch.py
@@ -1,10 +1,72 @@
 import numpy as np
 import pandas as pd
+import time
+from models import Model
+from record import create_record
 import matplotlib.pyplot as plt
 
 
-for ii in range(10):
-    if ii > 3:
-        if ii < 7:
-            continue
-    print(ii)
+record = create_record()
+odds_raw = pd.read_csv("data/odds.csv", header=None)
+
+dates_mdy = odds_raw[2].values
+dates_ymd = []
+for date in dates_mdy:
+    m, d, y = date.split('-')
+    dates_ymd.append('20' + y + '-' + m + '-' + d)
+odds_raw[2] = dates_ymd
+
+matchups_lvi = odds_raw[1].values
+
+
+## Format team names. LVI uses different abbreviations than fte for these three teams
+lvi2npw = {'BKN': 'BRK',
+           'CHA': 'CHO',
+           'PHX': 'PHO'}
+
+matchups_npw = []
+for idx, m_lvi in enumerate(matchups_lvi):
+
+    team1, team2 = m_lvi.split(" ")
+
+    if team1 in lvi2npw:
+        team1 = lvi2npw[team1]
+    if team2 in lvi2npw:
+        team2 = lvi2npw[team2]
+
+    matchups_npw.append(team1 + " " + team2)
+odds_raw[1] = matchups_npw
+
+
+odds_raw = odds_raw.set_index([2, 1]).drop(0, axis=1) ## Drops the column of rpi scrape-times
+
+matchups = np.unique(odds_raw.index)
+dates = []
+prediction_history = []
+for ii in range(0, matchups.shape[0], 2):
+
+    date = matchups[ii][0]
+    team1, team2 = matchups[ii][1].split(" ")
+    team1_ml = odds_raw.loc[date, team1 + " " + team2].iloc[-1].mean()
+    team2_ml = odds_raw.loc[date, team2 + " " + team1].iloc[-1].mean()
+    try:
+        winner = record.loc[matchups[ii][0], team2 + " " + team1]['winner']
+    except KeyError:
+        try:
+            winner = record.loc[matchups[ii][0], team1 + " " + team2]['winner']
+        except KeyError:
+            break
+
+    ## TODO - no fucking way this is right
+    if team1_ml < team2_ml:
+        projected_winner = team1
+    else:
+        projected_winner = team2
+
+    if projected_winner == winner:
+        prediction_history.append(1)
+    else:
+        prediction_history.append(0)
+    dates.append(date)
+
+lvi = Model(np.asarray(prediction_history), dates, "LVI")
